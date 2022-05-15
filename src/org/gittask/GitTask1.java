904087479543985b505733e63e1edc9f037fa6f2
Nayeem-ash <nayeemarshad03@gmail.com>
2022-05-15 11:56:02 +0530

    task done

---
+++ src/org/gittask/GitTask1.java
@@ -0,0 +1,16 @@
+package org.gittask;
+
+public class GitTask1 {
+	
+	private void add() {
+	System.out.println("adding");
+
+	}
+	
+	private void sub() {
+		System.out.println("sub");
+
+	}
+	
+
+}
